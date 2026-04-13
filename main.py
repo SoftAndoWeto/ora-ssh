aeb950ab7e0ddd8bc14c7cfca4d139c45bb867a9
Ilya <iosdsbb@gmail.com>
2026-04-13 12:25:05 +0300

    feat: updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from os import getenv
 from src.connection import connect
-from src.kex import send_kexinit, recv_kexinit
+from src.kex import send_kexinit, recv_kexinit, send_kexdh_init, recv_kexdh_reply, send_newkeys, recv_newkeys
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,3 +11,7 @@ s = connect(server_host)
 send_kexinit(s)
 payload = recv_kexinit(s)
 print("got KEXINIT, msg_type = ", payload[0])
+x, _ = send_kexdh_init(s)
+K, K_S, f, signature = recv_kexdh_reply(s, x)
+send_newkeys(s)
+recv_newkeys(s)
